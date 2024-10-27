a5b0109e2e708569618c114ee5a130c9651e8e6e
Madhusudhan Reddy <reddymadhu020@gmail.com>
2024-10-27 13:14:26 -0500

    Added Microphone feature

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaMicrophone } from 'react-icons/fa';
 import '../App.css';
 
 const ImageSearch = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleVoiceSearch = () => {
+    if ('webkitSpeechRecognition' in window) {
+      const recognition = new window.webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.lang = 'en-US';
+      recognition.interimResults = false;
+      recognition.maxAlternatives = 1;
+
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setSearchTerm(transcript); // Update search input with spoken text
+      };
+
+      recognition.onerror = (event) => {
+        console.error("Speech recognition error detected:", event.error);
+      };
+
+      recognition.start();
+    } else {
+      alert("Sorry, your browser doesn't support Speech Recognition.");
+    }
+  };
 
   const handleSearch = () => {
     onSearch(searchTerm);
@@ -11,12 +35,21 @@ const ImageSearch = ({ onSearch }) => {
   };
 
   return (
-    <div className="search-box-wrapper">
-      <div className="search-box">
+    <div
+      className="search-box-wrapper"
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+    >
+      <div className={`search-box ${isExpanded ? 'expanded' : ''}`}>
+      {isExpanded && (
+          <button onClick={handleVoiceSearch} className="mic-icon-button">
+            <FaMicrophone />
+          </button>
+        )}
         <input
           type="text"
           className="search-input"
-          placeholder="Type to search"
+          placeholder="Search"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           
